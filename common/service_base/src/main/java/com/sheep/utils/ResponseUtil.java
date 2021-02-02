f16483cf63d4801dd5723c896fce12baeaa28620
root <fangyang98@163.com>
2021-02-02 17:00:38 +0800

    Initial commit

---
+++ common/service_base/src/main/java/com/sheep/utils/ResponseUtil.java
@@ -0,0 +1,28 @@
+package com.sheep.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @ClassName ResponseUtil.java
+ * @author yangyangSheep
+ * @Description 返回内容通过Response进行返回
+ * @createTime 2021/2/2 16:45
+ */
+public class ResponseUtil {
+
+    public static void out(HttpServletResponse response, R r) {
+        ObjectMapper mapper = new ObjectMapper();
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try {
+            mapper.writeValue(response.getWriter(), r);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
